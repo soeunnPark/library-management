cff2ca3e382ae7dc83f2517c2a13fcb72dea9316
soeunnPark <carol0724@naver.com>
2025-04-12 21:16:18 +0900

    feat: divide test

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Assertions.*
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -16,4 +20,42 @@ class CalculatorTest {
             throw IllegalArgumentException()
         }
     }
+
+    fun minusTest() {
+        val calculator = Calculator(5)
+        calculator.minus(3)
+        if(calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun multiplyTest() {
+        val calculator = Calculator(5)
+        calculator.multiply(3)
+        if (calculator.number != 15) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideTest() {
+        val calculator = Calculator(5)
+        calculator.divide(2)
+        if(calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideExceptionTest() {
+        val calculator = Calculator(5)
+
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            return
+        } catch (e : Exception) {
+            throw IllegalArgumentException()
+        }
+        throw IllegalArgumentException("기대하는 예외가 발생하지 않았습니다. ")
+    }
+
 }
