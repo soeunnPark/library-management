f519d8b9180086d7b7cb47b0eb7629ddc6e8b0dd
soeunnPark <carol0724@naver.com>
2025-04-12 20:07:26 +0900

    feat: calculaor

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,21 @@
+package com.group.libraryapp.calculator
+
+import org.junit.jupiter.api.Assertions.*
+
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+}
+
+class CalculatorTest {
+    fun addTest() {
+        val calculator = Calculator(5)
+        calculator.add(3)
+
+        val expectedCalculator = Calculator(8)
+        if(calculator != expectedCalculator) {
+            throw IllegalArgumentException()
+        }
+    }
+}
